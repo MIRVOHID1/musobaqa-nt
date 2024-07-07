9b53b10025db64bb3d2f3240af3aac34975b86b4
MIRVOHID1 <toyirovmirvohid@gmail.com>
2024-07-07 13:13:36 +0300

    footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-import Footer from "./components/Footer/intex";
+import Footer from "./components/Footer/intdex";
 
 const inter = Inter({ subsets: ["latin"] });
 
